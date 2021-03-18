c96b70f15eeeccdb7421ff40be3837936abde25d
Kaloyan-Stankov <kalo944@gmail.com>
2021-03-19 01:55:32 +0200

    first commit

---
+++ src/main/java/com/stankov/orderbookwebsocket/OrderbookWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.stankov.orderbookwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderbookWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderbookWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stankov/orderbookwebsocket/OrderbookWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stankov.orderbookwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderbookWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
